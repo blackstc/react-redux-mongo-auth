464e6e212c5f8dfe732d87ffa638cc9245818152
Charlie Blackstock <charlieblackstock@gmail.com>
2016-03-17 14:13:48 -0600

    added user auth

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -5,12 +5,26 @@ exports.signup = function(req, res, next) {
     var email = req.body.email;
     var password = req.body.password;
 
-    User.findOne({ email: email }, function(err, user) {
-        
+    if (!email || !password) {
+        return res.status(422).send({ error: 'Must provide an email and password' });
+    }
+
+    User.findOne({ email: email }, function(err, existingUser) {
+        if (err) { return next(err); }
+
+        if (existingUser) {
+            return res.status(422).send({ error: 'Email is in use' });
+        }
+
+        var user = new User({
+            email: email,
+            password: password
+        });
+
+        user.save(function(err) {
+            if (err) { return next(err); }
+
+            res.json(user);
+        });
     });
-    // if user exists, return error
-
-    // no email then create and save a new user
-
-    // respond to request
 };
