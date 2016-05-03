eac675c742b562f8a1d0cf293273a60edd7ce790
Charlie Blackstock <charlieblackstock@gmail.com>
2016-05-03 15:24:17 -0600

    finished basic auth client side code

--- client/src/index.js
+++ client/src/index.js
@@ -4,18 +4,36 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import reduxThunk from 'redux-thunk';
+import { AUTH_USER } from './actions/types';
 
 import App from './components/app';
 import Signin from './components/auth/Signin';
+import Signout from './components/auth/Signout';
+import Signup from './components/auth/Signup';
+import Feature from './components/Feature';
+import Welcome from './components/Welcome';
+import RequireAuth from './components/auth/RequireAuth';
+
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+
+if (token) {
+  store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
-        <Route path="signin" component={Signin}></Route>
+        <IndexRoute component={Welcome} />
+        <Route path="signin" component={Signin} />
+        <Route path="signout" component={Signout} />
+        <Route path="signup" component={Signup}></Route>
+        <Route path="feature" component={RequireAuth(Feature)}></Route>
       </Route>
     </Router>
   </Provider>
